36bf9ff264d0477ddab28f25628e325dee40e542
YoonMooYeol <agecross@naver.com>
2025-02-19 12:04:25 +0900

    대문자로 변경

---
+++ hot6_pjt_ft/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<!-- src/components/navBar.vue -->
+<template>
+    <div class="nav-bar">
+      <span @click="goBack">◀</span>
+      <h1>🩵</h1>
+      <span>☰</span>
+    </div>
+  </template>
+  
+  <script setup>
+  const goBack = () => {
+    alert("뒤로 가기");
+  };
+  </script>
+  
+  <style scoped>
+  .nav-bar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background-color: #FFFFFF;
+    font-weight: bold;
+  }
+  </style>
