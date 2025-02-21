81e876bd9f4ba05dfeec8f0f39fd032487b2dd48
jyun5928 <jiyun5928@naver.com>
2025-02-21 17:04:09 +0900

    hot6_f4_6_1

--- hot6_pjt_ft/src/views/main.vue
+++ hot6_pjt_ft/src/views/main.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="home">
       <h1>Welcome to Chat App</h1>
-      <h2>화내지마 자기야 ♥︎</h2>
-      <h3>연인을 위한 따뜻한 메신저</h3>
       <div class="button-container">
         <router-link to="/female-chat" class="chat-button female">
           여성 채팅
@@ -26,24 +24,14 @@
     align-items: center;
     justify-content: center;
     height: 100vh;
-    background-color: #ffefef;
+    background-color: #d8faff;
   }
   
   h1 {
-    color: #ffffff;
-    margin-bottom: 2rem;
-    -webkit-text-stroke: 2px #BE7272;
-  }
-
-  h2 {
-    color: #BE7272;
-    margin-bottom: 1rem;
-  }
-
-  h3 {
-    color: #BE7272;
+    color: #333;
     margin-bottom: 2rem;
   }
+  
   .button-container {
     display: flex;
     gap: 1rem;
@@ -62,22 +50,12 @@
   }
   
   .female {
-    background-color: #ffd3de;
-    color: #5d5d5d;
+    background-color: #ffebf0;
+    color: #ff69b4;
   }
   
   .female:hover {
-    background-color: #BE7272;
-    color: white;
-  }
-
-  .male {
-    background-color: #CCE6FF;
-    color: #5d5d5d;
-  }
-  
-  .male:hover {
-    background-color: #88a9e7;
+    background-color: #ff69b4;
     color: white;
   }
   </style> 
