c5830c0445d71897a195b8986190a3ba9948a733
YoonMooYeol <agecross@naver.com>
2025-02-21 20:01:57 +0900

    디자인 수정

--- hot6_pjt_ft/src/views/femaleChat.vue
+++ hot6_pjt_ft/src/views/femaleChat.vue
@@ -8,7 +8,20 @@ import ChatContainer from "./ChatContainer.vue";
 
 <style scoped>
 .female-chat {
-  /* 여성 채팅 전용 mine 메시지 색상 등 커스터마이징 */
-  /* 이제 ChatContainer 내부의 deep 셀렉터로 적용됩니다. */
+  ::v-deep(.female-chat) .chat-container {
+    background-color: #F5F5F5;
+  }
+  ::v-deep(.female-chat) .chat-container.warm {
+    background-color: #ffe2e2;
+  }
+  ::v-deep(.female-chat) {
+    --mine-bubble-color: #ffebf0;
+  }
+  ::v-deep(.female-chat) .option-btn {
+  background: #d58080;
+  }
+  ::v-deep(.female-chat) .option-btn:hover {
+    background: #ce5d5d;
+  }
 }
 </style>
